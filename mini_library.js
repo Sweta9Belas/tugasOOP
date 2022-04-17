7005d8ec0ac160b747863bea3f428eac07fcf143
Sweta9Belas <71574994+Sweta9Belas@users.noreply.github.com>
2022-04-17 12:57:30 +0800

    upload file mini library pertama

---
+++ mini_library.js
@@ -0,0 +1,33 @@
+new gridjs.Grid({
+  search: true,
+  sort: true,
+  pagination: {
+    limit: 5,
+  },
+  language: {
+    search: {
+      placeholder: "🔍 ketik kata kunci...",
+    },
+    pagination: {
+      previous: "Sebelumnya",
+      next: "Berikutnya",
+      showing: "Menampilkan",
+      to: "sampai",
+      of: "dari",
+      results: "Data",
+    },
+  },
+  columns: ["Judul", "Tahun Rilis", "IMDb Rating"],
+  data: [
+    ["Transformers", "28 Juni 2007", "7.0/10"],
+    ["Transformers : Revenge of the Fallen", "24 Juni 2009", "5.9/10"],
+    ["Transformers : Dark of the Moon", "05 Agustus 2011", "6.2/10"],
+    ["Transformers : Age of Extinction", "25 Juni 2014", "5.6/10"],
+    ["Transformers : The Last Knight", "21 Juni 2017", "5.2/10"],
+    ["Bumblebee", "19 Desember 2018", "6.7/10"],
+    ["Snowden", "16 September 2016", "7.3/10"],
+    ["Fantastic Beasts and Where to Find Them", "16 November 2016", "7.2/10"],
+    ["Fantastic Beasts: The Crimes of Grindelwald", "14 November 2018", "6.2/10"],
+    ["Fantastic Beasts: The Secrets of Dumbledore", "13 April 2022", "6.6/10"],
+  ],
+}).render(document.getElementById("basecamp"));
