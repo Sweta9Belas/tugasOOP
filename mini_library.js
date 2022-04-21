be744b3c9f3e9215fc39525530cc9725080b5ad3
Sweta9Belas <71574994+Sweta9Belas@users.noreply.github.com>
2022-04-21 21:11:55 +0800

    update mini library.js

--- mini_library.js
+++ mini_library.js
@@ -1,22 +1,42 @@
-new gridjs.Grid({
-  search: true,
-  sort: true,
-  pagination: {
-    limit: 5,
-  },
-  language: {
-    search: {
-      placeholder: "🔍 ketik kata kunci...",
-    },
-    pagination: {
-      previous: "Sebelumnya",
-      next: "Berikutnya",
-      showing: "Menampilkan",
-      to: "sampai",
-      of: "dari",
-      results: "Data",
-    },
-  },
+class Table {
+  constructor(init) {
+    this.init = init;
+  }
+
+  createHeader(data) {
+    let open = "<thead><tr>";
+    let close = "</tr></thead>";
+    data.forEach((d) => {
+      open += `<th class='table-warning text-center'>${d}</th>`;
+    });
+
+    return open + close;
+  }
+
+  createBody(data) {
+    let open = "<tbody>";
+    let close = "</tbody>";
+
+    data.forEach((d) => {
+      open += `
+        <tr class='table-dark' >
+          <td class='table-warning'>${d[0]}</td>
+          <td class='table-info'>${d[1]}</td>
+          <td class='table-danger'>${d[2]}</td>
+        </tr>
+      `;
+    });
+
+    return open + close;
+  }
+
+  render(element) {
+    let table = "<table class='table table-bordered border-warning table-light table-striped table-hover' style=''>" + this.createHeader(this.init.columns) + this.createBody(this.init.data) + "</table>";
+    element.innerHTML = table;
+  }
+}
+
+const table = new Table({
   columns: ["Judul", "Tahun Rilis", "IMDb Rating"],
   data: [
     ["Transformers", "28 Juni 2007", "7.0/10"],
@@ -30,4 +50,6 @@ new gridjs.Grid({
     ["Fantastic Beasts: The Crimes of Grindelwald", "14 November 2018", "6.2/10"],
     ["Fantastic Beasts: The Secrets of Dumbledore", "13 April 2022", "6.6/10"],
   ],
-}).render(document.getElementById("basecamp"));
+});
+const basecamp = document.getElementById("basecamp");
+table.render(basecamp);
--- table.js
+++ table.js
@@ -1,40 +1,40 @@
-// class Table {
-//   constructor(init) {
-//     this.init = init;
-//   }
+class Table {
+  constructor(init) {
+    this.init = init;
+  }
 
-//   createHeader(data) {
-//     let open = "<thead><tr>";
-//     let close = "</tr></thead>";
-//     data.forEach((d) => {
-//       open += `<th>${d}</th>`;
-//     });
+  createHeader(data) {
+    let open = "<thead><tr>";
+    let close = "</tr></thead>";
+    data.forEach((d) => {
+      open += `<th>${d}</th>`;
+    });
 
-//     return open + close;
-//   }
+    return open + close;
+  }
 
-//   createBody(data) {
-//     let open = "<tbody>";
-//     let close = "</tbody>";
+  createBody(data) {
+    let open = "<tbody>";
+    let close = "</tbody>";
 
-//     data.forEach((d) => {
-//       open += `
-//         <tr>
-//           <td>${d[0]}</td>
-//           <td>${d[1]}</td>
-//           <td>${d[2]}</td>
-//         </tr>
-//       `;
-//     });
+    data.forEach((d) => {
+      open += `
+        <tr>
+          <td>${d[0]}</td>
+          <td>${d[1]}</td>
+          <td>${d[2]}</td>
+        </tr>
+      `;
+    });
 
-//     return open + close;
-//   }
+    return open + close;
+  }
 
-//   render(element) {
-//     let table = "<table class='table table-hover'>" + this.createHeader(this.init.columns) + this.createBody(this.init.data) + "</table>";
-//     element.innerHTML = table;
-//   }
-// }
+  render(element) {
+    let table = "<table class='table table-hover'>" + this.createHeader(this.init.columns) + this.createBody(this.init.data) + "</table>";
+    element.innerHTML = table;
+  }
+}
 
 const table = new Table({
   columns: ["Name", "Email", "Alamat"],
